93c7e12d227ae79b13dd0b0c330ced791567f508
Kazim Ozdil <skozdil@gmail.com>
2024-03-15 21:43:35 +0000

    updated resume page

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -4,10 +4,9 @@ export default function Home() {
       <section className="container-fluid home">
         <div className="container jumbotron">
           <h1 className="display-1 hello-big">Hello!</h1>
-          <h1 className="display-3">I am {myResume.name}!</h1>
-          <p className="lead">I am a {myResume.role}.{myResume.roleDescription}</p>
+          <h1 className="display-3">I am {myResume.name}</h1>
           <hr className="my-4" />
-          <p>A passionate front-end web developer with a keen eye for creating visually stunning and intuitive user experiences.</p>
+          <p className="lead">I am a {myResume.role}. {myResume.roleDescription}</p>
           <p className="lead">
             <a className="btn btn-primary btn-lg" href="/about" role="button">Learn more</a>
           </p>
--- src/components/pages/Resume.jsx
+++ src/components/pages/Resume.jsx
@@ -2,84 +2,74 @@ import myResume from '/myresume';
 export default function Resume () {
     return (
       <section id="resume" className="container resume">
-         <div className="row education">
-            <div className="col">
-               <h1><span>Education</span></h1>
-            </div>
-            <div className="col">
-              {
-               myResume.education.map((item)=>{
-                  return(
-                    <div className="row">
-                       <div className="col">
-                          <h3>{item.UniversityName}</h3>
-                          <p className="info">
-                          {item.specialization}
-                          <span>&bull;</span> <em className="date">{item.MonthOfPassing} {item.YearOfPassing}</em></p>
-                          <p>
-                          {item.Achievements}
-                          </p>
-                       </div>
-                    </div>
-                  )
-                })
-              }
-            </div>
-         </div>
-        <div className="row">
-            <div className="columns">
-               <h1><span>Work</span></h1>
-            </div>
-
-            <div className="col">
-              {
-                myResume.work.map((item) => {
-                  return(
-                    <div className="row">
-                       <div className="col">
-                          <h3>{item.CompanyName}</h3>
-                          <p className="info">
-                          {item.specialization}
-                          <span>&bull;</span> <em className="date">{item.MonthOfLeaving} {item.YearOfLeaving}</em></p>
-                          <p>
-                          {item.Achievements}
-                          </p>
-                       </div>
-
-                    </div>
-
-                  )
-                })
-              }
-            </div> 
-         </div>
          <div className="row">
-            <div className="col">
-               <h1><span>Skills</span></h1>
+            <div className="col-lg-6">
+               <div className="col-4 main-title">
+                  <h3>Education</h3>
+               </div>
+               <div className="col-8">
+                  {
+                     myResume.education.map((item)=>{
+                        return(
+                        <div className="row">
+                           <div className="col title">
+                              <h4>{item.UniversityName}</h4>
+                              <p className="info">{item.specialization} - <span className="date">{item.MonthOfPassing} {item.YearOfPassing}</span></p>
+                              <p>{item.Achievements}</p>
+                           </div>
+                        </div>
+                        )
+                     })
+                  }
+               </div>
             </div>
-            <div className="col">
-               <p>{myResume.skillsDescription}</p>
+            
+            <div className="col-lg-6">
+               <div className="col-4 main-title">
+                  <h3>Work</h3>
+               </div>
+               <div className="col-8">
+                  {
+                     myResume.work.map((item) => {
+                        return(
+                        <div className="row">
+                           <div className="col title">
+                              <h4>{item.CompanyName}</h4>
+                              <p className="info">{item.specialization} - <span className="date">{item.MonthOfLeaving} {item.YearOfLeaving}</span></p>
+                              <p>{item.Achievements}</p>
+                           </div>
+                        </div>
 
-   				<div className="row">
-   				   <ul className="col skills">
-                {
-                  myResume.skills.map((item) => {
-                    return(
-                      <li>
-                      <span className={`skill-counter ${item.skillname.toLowerCase()}`}>
-                      </span><em>{item.skillname}</em>
-                      </li>
-                    )
-                  })
-                }
-
-   					</ul>
-
-   				</div>
-
-   			</div>
-
+                        )
+                     })
+                  }
+               </div> 
+            </div>
          </div>
+         <div className="container-fluid">         
+            <div className="col">
+               <div className="col-4 main-title">
+                  <h1><span>Skills</span></h1>
+               </div>
+               <div className="col-8">
+                  <p>{myResume.skillsDescription}</p>
+                  <div className="row">
+                     <ul className="list-group skill-list">
+                        {
+                           myResume.skills.map((item) => {
+                           return(
+                              <li className="list-group-item d-flex justify-content-between align-items-center">
+                                 {item.skillname}
+                                 <span class="badge badge-primary badge-pill">14</span>
+                              </li>
+
+                           )
+                           })
+                        }
+                     </ul>
+                  </div>
+               </div>
+            </div></div>
 
       </section>
     );
